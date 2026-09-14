089dc77808e134055f719dddcff3e8269e1027c4
migsaito <193292531+migsaito@users.noreply.github.com>
2026-09-14 13:26:21 +0000

    refactor: rename script and internal process to unknown_process

--- unknown_process.py
+++ unknown_process.py
@@ -8,7 +8,7 @@ import multiprocessing
 signal.signal(signal.SIGINT, signal.SIG_IGN)
 signal.signal(signal.SIGTERM, signal.SIG_IGN)
 
-def set_proc_name(name="miguel"):
+def set_proc_name(name="unknown_process"):
     try:
         libc = ctypes.CDLL('libc.so.6')
         libc.prctl(15, name.encode('utf-8') + b'\0', 0, 0, 0)
@@ -18,7 +18,7 @@ def set_proc_name(name="miguel"):
 def cpu_workload(target_ratio=0.20, interval=0.05):
     signal.signal(signal.SIGINT, signal.SIG_IGN)
     signal.signal(signal.SIGTERM, signal.SIG_IGN)
-    set_proc_name("miguel")
+    set_proc_name("unknown_process")
     
     busy_time = interval * target_ratio
     sleep_time = interval * (1.0 - target_ratio)
@@ -30,7 +30,7 @@ def cpu_workload(target_ratio=0.20, interval=0.05):
         time.sleep(sleep_time)
 
 if __name__ == '__main__':
-    set_proc_name("miguel")
+    set_proc_name("unknown_process")
 
     target_ram_mb = 3072
     memory_block = bytearray(b'x' * (target_ram_mb * 1024 * 1024))
